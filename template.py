9bfd525a06406b21083cbf0b09657b0c62a9cc4d
Adarsh2004ku <adarsh2022ji@gmail.com>
2025-12-11 01:11:34 +0530

    template issue fixed

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO,format = '[%(asctime)s]: %(message)s:')
 
 project_name = "datascience"
 list_of_files = [
-    ".github/workflows/.gitkeep",
+    ".gthub/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -15,6 +15,8 @@ list_of_files = [
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
@@ -23,7 +25,8 @@ list_of_files = [
     "Dockerfile",
     "setup.py",
     "research/research.ipynb",
-    "templates/index.html",]
+    "templates/index.html",
+    "app.py"]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
